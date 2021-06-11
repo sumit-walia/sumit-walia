dde7c90462418dd238b7ee209b966c2bd3a55694
sumit-walia <walia.sumit76@gmail.com>
2021-06-11 20:50:13 +0530

    New Website

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// const news = document.querySelectorAll(".news-item");
+// // console.log(news)
+
+// function removeActiveClass(news) {
+//     for (const news_ of news) {
+//         // console.log(news_);
+//         news_.classList.remove("active")
+//     }   
+// }
+
+// let k=0
+// while (k<3) {
+//     removeActiveClass(news)
+//     news[0].classList.add("active");
+//     k++;
+//     if(k==3){
+//         k=0;
+//     }
+// }
